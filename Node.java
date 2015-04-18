21a6944b58c045001a52783fdf3fbdf1f2457a53
Anis Kallel <akallel@users.noreply.github.com>
2015-04-18 11:27:28 -0400

    added toString method

--- Node.java
+++ Node.java
@@ -1,8 +1,11 @@
 public class Node{
-		int X;
-		int Y;
-		public Node(int X, int Y){
-			this.X=X;
-			this.Y=Y;
-		}
+	int X;
+	int Y;
+	public Node(int X, int Y){
+		this.X=X;
+		this.Y=Y;
 	}
+	public String toString(){
+		return "("+ X + "," + Y + ")";
+	}	
+}
